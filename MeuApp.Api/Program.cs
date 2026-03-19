643b177b030ee3b28089c725539977641e87e1a8
julioCAlmeida <julioce.almeid@gmail.com>
2026-03-19 09:25:50 -0300

    feat: refactoring program.cs

--- MeuApp.Api/Program.cs
+++ MeuApp.Api/Program.cs
@@ -1,8 +1,10 @@
+using MeuApp.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<CalculatorService>();
 
 var app = builder.Build();
 
@@ -14,28 +16,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/calculate/sum", (int a, int b, CalculatorService service) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return Results.Ok(service.Sum(a, b));
+});
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/calculate/divide", (int a, int b, CalculatorService service) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    try
+    {
+        return Results.Ok(service.Divide(a, b));
+    }
+    catch (ArgumentException ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapGet("/calculate/check", (int number, CalculatorService service) =>
+{
+    return Results.Ok(service.CheckNumber(number));
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
